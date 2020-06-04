2fe8e7e4ebeb66c3b88d4ee894ab68e9c8450821
Harsh Gandhi <63015502+Harshgandhi29@users.noreply.github.com>
2020-06-04 14:24:34 -0400

    Update arduino.py

--- arduino.py
+++ arduino.py
@@ -6,12 +6,9 @@ def light(port,pin):
 
     iterator = util.Iterator(board)
     iterator.start()
-    #pin = int(pin)
     Tv1 = board.get_pin(f'd:{pin}:o')
 
     time.sleep(1.0)
     Tv1.write(1)
     print(Tv1.read())
-   #time.sleep(4.0)
-   # Tv1.write(0)
     board.exit()
